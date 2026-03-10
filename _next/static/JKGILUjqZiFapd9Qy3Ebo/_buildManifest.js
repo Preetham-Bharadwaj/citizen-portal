788b8dffca104a6c8e495b070f8b0f5505f5f38e
Preetham-Bharadwaj <preethambharadwajbr@gmail.com>
2026-03-10 16:38:38 +0530

    Updates

--- _next/static/JKGILUjqZiFapd9Qy3Ebo/_buildManifest.js
+++ _next/static/JKGILUjqZiFapd9Qy3Ebo/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/citizen-portal//_next/:path+",
+        "destination": "/citizen-portal/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
